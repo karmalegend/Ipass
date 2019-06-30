5598865104283e61a2a3e90ee3422f45e39bd0a8
karmalegend <bart26nl@gmail.com>
2019-06-30 23:43:18 +0200

    fixed heroku db link and pom.xml configuration

--- src/main/java/nl/hu/ipass/project/persistance/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/project/persistance/PostgresBaseDao.java
@@ -3,9 +3,10 @@ package nl.hu.ipass.project.persistance;
 import java.sql.*;
 
 public class PostgresBaseDao  {
-    protected static final String USER = "postgres";
-    protected static final String PASS = "b1b2b3b4";
-    protected static final String URL = "jdbc:postgresql://localhost:5432/ipass";
+    protected static final String USER = "omlpmnzbohximz";
+    protected static final String PASS = "335ff82bc930462afd03cb30507941d807b3f46f729ba6f00f1911826179a4dd";
+    protected static final String URL = "jdbc:postgresql://ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d1clal5csmkaor";
+
 
     protected static Connection getConnection(){
         try{
