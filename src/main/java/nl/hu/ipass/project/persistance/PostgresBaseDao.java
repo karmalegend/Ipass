28521d2b1803602b23357cfc722304d879af652f
karmalegend <43471135+karmalegend@users.noreply.github.com>
2020-02-25 21:00:39 +0100

    Update PostgresBaseDao.java
    should've fixed connection properties

--- src/main/java/nl/hu/ipass/project/persistance/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/project/persistance/PostgresBaseDao.java
@@ -4,9 +4,9 @@ package nl.hu.ipass.project.persistance;
 import java.sql.*;
 
 public class PostgresBaseDao  {
-    protected static final String USER = "bhasjvqwkedpuz";
-    protected static final String PASS = "642a9e039d6da5080958464b5f84b89b7ade69f900740999e2d6cdc2ec046dee";
-    protected static final String URL = "jdbc:postgresql://ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/dtcgokk40hdis";
+    protected static final String USER = "prchsskzaglrhj";
+    protected static final String PASS = "bf7a8ef22d301e845c95b3262fbc23516d6b02aa0d113b1b88c989ba95c665f1";
+    protected static final String URL = "jdbc:postgresql://ec2-54-247-189-1.eu-west-1.compute.amazonaws.com:5432/d3ht24rb2redi7";
 
     /*
     *
